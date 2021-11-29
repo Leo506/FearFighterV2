d5469d115ea704a7aeeb8ffbb0630b71a429a5ee
Artem <leoartemlev@yandex.ru>
2021-11-30 00:15:55 +0300

    Сделал вывод фраз босса из файла в ui

---
+++ Assets/Scripts/BossFight/UIController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] Text bossText;
+    [SerializeField] BossTextLoader loader;
+
+    int currentPhrase = 0;
+
+    void Update() {
+    	if (Input.GetMouseButtonDown(0)) {
+    		if (loader.isLoaded) {
+    			StopAllCoroutines();
+    			ShowPhrase();
+    		}
+    	}
+    }
+
+    public void ShowPhrase() {
+    	if (loader.isLoaded) {
+    		string text;
+    		if (currentPhrase == 0)
+    			text = loader.bossText["_StartDialog"];
+    		else
+    			text = loader.bossText[$"_Clue{currentPhrase}"];
+
+    		StartCoroutine(Show(text));
+
+    		currentPhrase++;
+    	}
+    }
+
+    IEnumerator Show(string text) {
+    	string tmp = "";
+    	foreach (var item in text) {
+    		tmp += item;
+    		bossText.text = tmp;
+    		yield return new WaitForSeconds(0.1f);
+    	}
+    }
+}
