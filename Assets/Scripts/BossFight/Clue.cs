809c9de0343332b36ba7d80dca5cd2a9293e2795
Artem <leoartemlev@yandex.ru>
2021-12-01 19:35:36 +0300

    Добавил урон по боссу, по игроку и фразы босса

--- Assets/Scripts/BossFight/Clue.cs
+++ Assets/Scripts/BossFight/Clue.cs
@@ -3,50 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Clue : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class Clue : MonoBehaviour, IPointerDownHandler, IEndDragHandler, IDragHandler
 {
-	[SerializeField] Canvas canvas;         // Canvas, в котором происходит перемещение объекта
-	[SerializeField] RectTransform target;  // Куда нужно перетащить объект
-	[SerializeField] UIController uiController;
+	[SerializeField] Canvas canvas;              // Canvas, в котором происходит перемещение объекта
+	[SerializeField] RectTransform target;       // Куда нужно перетащить объект
+	[SerializeField] Boss boss;                  // Объект босса
+	public int clueID;                 			 // ID улики
 
 	Vector3 startPos;
 
     private RectTransform rectTransform;
 
     private void Awake() {
-    	rectTransform = GetComponent<RectTransform>();
-    	startPos = transform.localPosition;
-    }
 
+    	rectTransform = GetComponent<RectTransform>(); // Получаем компонент RectTransform, через который будем перемещать улику
 
-    public void OnBeginDrag(PointerEventData eventData) {
-    	Debug.Log("Begin drag");
+    	startPos = transform.localPosition;            // Запоминаем изначальную позицию улики
     }
 
 
     public void OnDrag(PointerEventData eventData) {
     	Debug.Log("On drag");
-    	rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    	rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;  // Перемещаем улику куда укажет игрок
     }
 
 
     public void OnEndDrag(PointerEventData eventData) {
     	Debug.Log("End drag");
+
+    	// Возвращаем прежний размер, тк игрок перестал передвигать улику
     	transform.localScale = new Vector3(1, 1, 1);
 
+    	// Проверяем, попала ли улика на объект, на который нужно её перенести
+    	// Сначала проверяем координаты по Х,
+    	// потом по Y
     	if (transform.position.x >= target.position.x - target.rect.width / 2 && transform.position.x <= target.position.x + target.rect.width / 2) {
     		if (transform.position.y >= target.position.y - target.rect.height / 2 && transform.position.y <= target.position.y + target.rect.height / 2) {
-    			uiController.ShowPhrase();
-    			Destroy(this.gameObject);
+    			
+    			// Пытаемся атаковать босса
+    			if (boss.TryAttack(clueID))   
+    				Destroy(this.gameObject);
     		}
     	}
 
+    	// Если не попали на цель, то возвращаем улику на своё место
     	transform.localPosition = startPos;
     }
 
 
     public void OnPointerDown(PointerEventData eventData) {
     	Debug.Log("Pointer down");
+
+    	// Увеличиваем размер улики, чтобы показать, что она выбрана
     	transform.localScale = new Vector3(1.5f, 1.5f, 1);
     }
 }
--- Assets/Scripts/BossFight/UIController.cs
+++ Assets/Scripts/BossFight/UIController.cs
@@ -8,40 +8,73 @@ public class UIController : MonoBehaviour
     [SerializeField] Text bossText;
     [SerializeField] BossTextLoader loader;
 
-    int currentPhrase = 0;
+    Queue<string> requireIDs = new Queue<string>();  // Очередь со всеми id, которые нужно показать
+
+    bool isShowing = false;
+
 
     void Start() {
     	StartCoroutine(WaitUntilLoad());
     }
 
-    public void ShowPhrase() {
+
+    /// <summary>Добавляет id к списку id'шников, которые нужно показать</summary>
+    /// <param name="id">id фразы</param>
+    public void RegisterNextID(string id) {
+
+    	requireIDs.Enqueue(id);
+
+    	if (!isShowing && loader.isLoaded)
+    		StartShow();
+    }
+
+
+
+    /// <summary>Начать вывод фраз</summary>
+    public void StartShow() {
+    	if (requireIDs.Count != 0)
+    		ShowPhrase(requireIDs.Dequeue());
+    }
+
+    /// <summary>Препывает вывол фраз</summary>
+    public void StopShow() {
+    	StopAllCoroutines();
+    }
+
+
+    /// <summary>Показывает следующую фразу босса</summary>
+    /// <param name="phraseID">ID фразы</param>
+    public void ShowPhrase(string phraseID) {
+
     	if (loader.isLoaded) {
-    		string text;
-    		if (currentPhrase == 0)
-    			text = loader.bossText["_StartDialog"];
-    		else if (currentPhrase < loader.bossText.Count)
-    			text = loader.bossText[$"_Clue{currentPhrase}"];
-    		else
-    			text = "...";
+    		string text = loader.bossText[phraseID];
 
     		StartCoroutine(Show(text));
-
-    		currentPhrase++;
     	}
+
     }
 
     IEnumerator Show(string text) {
+
+    	isShowing = true;
+    	
     	string tmp = "";
     	foreach (var item in text) {
     		tmp += item;
     		bossText.text = tmp;
     		yield return new WaitForSeconds(0.1f);
     	}
+
+    	if (requireIDs.Count != 0)
+    		ShowPhrase(requireIDs.Dequeue());
+    	else
+    		isShowing = false;
     }
 
 
     IEnumerator WaitUntilLoad() {
     	yield return new WaitUntil(() => loader.isLoaded);
-    	ShowPhrase();
+    	if (requireIDs.Count != 0)
+    		ShowPhrase(requireIDs.Dequeue());
     }
 }
