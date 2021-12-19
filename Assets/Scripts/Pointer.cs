ca3f2746dce7b4dcf10a52ceae56b6a1ecde2df5
Artem <leoartemlev@yandex.ru>
2021-12-19 21:53:42 +0300

    Добавил указатель на портал

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour, ISetUpObj
+{
+    public Transform target; // объект за которым надо следить
+
+    public SpriteRenderer sprite;
+
+
+    public void SetUp()
+    {
+        target = FindObjectOfType<Portal>().transform;
+    }
+    
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        Debug.Log(dir);
+        var zRot = Mathf.Acos(Vector3.Dot(Vector3.forward, dir) / dir.magnitude) * Mathf.Rad2Deg;
+
+        if (Vector3.Cross(Vector2.up, dir).z < 0)
+            zRot *= -1;
+        
+        transform.rotation = Quaternion.Euler(90, 0, zRot);
+    }
+}
