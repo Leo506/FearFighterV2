01cb3318bc4d3d6bf717a6ad105214b216b041c0
Artem <leoartemlev@yandex.ru>
2022-01-26 17:00:22 +0300

    Автоматический подбор предметов

--- Assets/Scripts/DroppingItems/DroppingObj.cs
+++ Assets/Scripts/DroppingItems/DroppingObj.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DroppingObj : MonoBehaviour, IUsingObj
+public class DroppingObj : MonoBehaviour
 {
+    protected Transform playerTransform;
+    protected bool canMove = false;
+    public float speed = 10.0f;
+
+    private void Update() 
+    {
+        if (canMove)
+        {
+            this.transform.Translate((playerTransform.position - this.transform.position).normalized * speed * Time.deltaTime);    
+            if (Vector3.Distance(playerTransform.position, this.transform.position) <= 0.1f)
+                OnGet();
+        }
+    }
+
+
+    /// <summary>
+    /// Начать движение
+    /// </summary>
+    public void StartMove(Transform transform)
+    {
+        canMove = true;
+        playerTransform = transform;
+    }
 
     protected virtual void OnGet()
     {
         Debug.Log("Использование дропа");
         Destroy(this.gameObject);
-    }
-
-    public void Use()
-    {
-        OnGet();
     }
 }
