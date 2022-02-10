6fab225f8ff6ec9b6e5c4659bf5a8fc543acb235
Artem <leoartemlev@yandex.ru>
2022-02-10 12:56:10 +0500

    Тест загрузки префаба из Asset Bundle

---
+++ Assets/Editor/CreateAssetsBundles.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class CreateAssetsBundles
+{
+    [MenuItem("Assets/Create All Asset Bundles")]
+    static void CreateAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/StreamingAssets/GameMaps";
+        if (!Directory.Exists(assetBundleDirectory))
+            Directory.CreateDirectory(assetBundleDirectory);
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+}
---
+++ Assets/Scripts/MapLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MapLoader : MonoBehaviour
+{
+    public string assetName = "Map0";
+    public string bundleName = "StreamingAssets";
+
+    public void Start()
+    {
+        AssetBundle localBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, bundleName));
+
+        if (localBundle == null)
+        {
+            Debug.LogError("Failed to load asset bundle");
+            return;
+        }
+
+        GameObject asset = localBundle.LoadAsset<GameObject>(assetName);
+        Instantiate(asset);
+        localBundle.Unload(false);
+    }
+}
