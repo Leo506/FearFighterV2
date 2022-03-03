40e58dbcbcf717b678ccea3589a735887f8cedb0
Artem <leoartemlev@yandex.ru>
2022-03-03 15:35:33 +0300

    Добавил UI достижения

---
+++ Assets/Scripts/AchievementsSystem/AchievementItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementItem : MonoBehaviour
+{
+    [SerializeField] Image achieveImage;  // Изображение достижения
+    [SerializeField] Button getButton;    // Кнопка получения награды за достижение
+
+    public int id;                        // Id достижения, которое отображает данный UI item
+
+    /// <summary>
+    /// Обновляет вид достижения на экране
+    /// </summary>
+    /// <param name="fill">Процент прогресса достижения</param>
+    /// <param name="enable">Доступно ли получение награды</param>
+    public void UpdateView(float fill, bool enable)
+    {
+        achieveImage.fillAmount = fill;
+        getButton.enabled = enable;
+    }
+
+
+    /// <summary>
+    /// Получение награды
+    /// </summary>
+    public void GetAward()
+    {
+        AchievementManager.GetAchievementById(id)?.GetAward();
+    }
+}
---
+++ Assets/Scripts/AchievementsSystem/AchievementsUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementsUI : MonoBehaviour
+{
+    [SerializeField] AchievementItem[] items;  // UI items достижений
+
+    public void UpdateUI()
+    {
+        foreach (var item in items)
+        {
+            Achievement? tmp = AchievementManager.GetAchievementById(item.id);
+            if (tmp.HasValue)
+            {
+                Achievement achievement = tmp.Value;
+                item.UpdateView(achievement.currentProgress, achievement.IsGot);
+            }
+        }
+    }
+}
