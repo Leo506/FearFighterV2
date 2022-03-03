6910b5408a77b280aadeb13d2c1bdee4306b8d58
Artem <leoartemlev@yandex.ru>
2022-03-03 14:59:07 +0300

    Создал структуру, представляющую достижение и менеджер достижений

---
+++ Assets/Scripts/AchievementsSystem/Achievement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public struct Achievement
+{
+	/// <summary>
+	/// Значение тукущего прогресса достижения
+	/// </summary>
+	public float currentProgress { get; private set; }                // Значение текущего прогресса (от 0 до 1)
+
+
+	/// <summary>
+	/// Id достижения
+	/// </summary>
+	/// <value></value>
+	public int id { get; private set; }                               // Id достижения
+
+
+	/// <summary>
+	/// Событие получение события
+	/// </summary>
+	public static event Action<Achievement> AchievementGotEvent; 
+
+	public bool IsGot { get { return currentProgress == 1; } }        // Полученно ли достижение?
+
+	private Func<float> progressFunction;                             // Функция, возвращающая текущий прогресс по достижению
+
+	private int awardAmount;                                          // Величина награды
+	private bool awardGot;                                            // Получена ли награда
+
+
+	/// <summary>
+	/// Создаёт экземпляр структуры Achievement
+	/// </summary>
+	/// <param name="progress">Функция, определяющая прогресс по достижению</param>
+	/// <param name="id">Id достижения</param>
+	/// <param name="award">Величина награды</param>
+	public Achievement(Func<float> progress, int id, int award)
+	{
+		progressFunction = progress;
+		currentProgress = progressFunction();
+		this.id = id;
+		awardAmount = award;
+		awardGot = false;
+	}
+
+
+	/// <summary>
+	/// Получение награды
+	/// </summary>
+	public void GetAward()
+	{
+		if (!awardGot)
+			Debug.Log("Получена награда");
+		else
+			Debug.Log("Награда была получена ранее");
+	}
+}
---
+++ Assets/Scripts/AchievementsSystem/AchievementManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AchievementManager
+{
+    // Список достижений
+    static Achievement[] achievements = new Achievement[] 
+    {
+        new Achievement(() => { return 1.0f; }, 0, 100),
+        new Achievement(() => { return 0.5f; }, 1, 500)
+    };
+
+
+    /// <summary>
+    /// Получить достижение по его id
+    /// </summary>
+    /// <param name="id">id искомого достижения</param>
+    /// <returns>Достижение с опр id</returns>
+    public static Achievement? GetAchievementById(int id)
+    {
+        var array = achievements.Where(a => a.id == id).ToArray();
+        
+        if (array.Length == 0)
+            return null;
+        else
+            return array[0];
+    }
+}
