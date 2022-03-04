c56f30de4210534c13f2609ae06c0c44a0c2e381
Artem <leoartemlev@yandex.ru>
2022-03-04 15:33:26 +0300

    Добавил менеджер денег и опыта игрока

---
+++ Assets/Scripts/PlayerResources/ExpController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpController
+{
+    static int exp = 0;  // Кол-во опыта у игрока
+
+
+    /// <summary>
+    /// Количество опыта у игрока
+    /// </summary>
+    /// <value></value>
+    public int Exp
+    {
+        get
+        {
+            return exp;
+        }
+
+        private set
+        {
+            if (value >= 0)
+                exp = value;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerResources/MoneyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyController
+{
+	static int money = 0;  // Кол-во монет у игрока
+
+	/// <summary>
+	/// Количество денег у игрока
+	/// </summary>
+	/// <value></value>
+	public static int Money
+	{
+		get
+		{
+			return money;
+		}
+
+		private set
+		{
+			if (value >= 0)
+				money = value;
+		}
+	}
+}
